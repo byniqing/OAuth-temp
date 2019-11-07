1436d699744c824f9db0225a7957b9eed50eb264
byniqing <byniqing@163.com>
2019-11-07 20:18:55 +0800

    ....

---
+++ IdentityServer/Models/ApplicationUseAuthorization.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer.Models
+{
+    /// <summary>
+    /// 用户授权第三方登录的信息
+    /// </summary>
+    public class ApplicationUseAuthorization
+    {
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 关联客户端的id
+        /// </summary>
+        public int ClientId { get; set; }
+
+        /// <summary>
+        /// 当用取消授权后，置为false(0)
+        /// </summary>
+        public bool Enabled { get; set; }
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime Created { get; set; }
+    }
+}
---
+++ Info/Common/Utils.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Info.Common
+{
+    public static class Utils
+    {  /// <summary>
+       /// 获取时间毫秒数
+       /// 提取为通用方法
+       /// </summary>
+       /// <returns></returns>
+        public static long GetTimeStamp()
+        {
+            //return (long)(DateTime.Parse(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")).Subtract(new DateTime(1970, 1, 1, 0, 0, 0)).TotalMilliseconds);
+            DateTime Jan1st1970 = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            return (long)(DateTime.Now.AddHours(-8) - Jan1st1970).TotalMilliseconds;
+
+        }
+        /// <summary>
+        /// 时间戳转换为日期（时间戳单位秒）
+        /// </summary>
+        /// <param name="TimeStamp"></param>
+        /// <returns></returns>
+        public static DateTime ConvertToDateTime(long timeStamp)
+        {
+            var start = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            return start.AddMilliseconds(timeStamp).AddHours(8);
+        }
+
+    }
+}
