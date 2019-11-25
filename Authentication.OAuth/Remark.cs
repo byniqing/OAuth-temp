5ee1c58e1edad0ebccec3c72278b59409ad5f252
byniqing <byniqing@163.com>
2019-11-25 17:07:10 +0800

    ...

---
+++ Authentication.OAuth/Remark.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Authentication.OAuth
+{
+    public class Remark
+    {
+        /*
+         * 参考：
+         * https://www.cnblogs.com/lori/p/8183363.html?utm_source=tuicool&utm_medium=referral
+         * https://www.cnblogs.com/wangshitou/p/10278631.html
+         把net core 库打包成nuget包
+         在程序包管理控制台
+          dotnet pack Authentication.OAuth\Authentication.OAuth.csproj -o nupkg /p:Version=1.0.0
+         */
+    }
+}
